3eb9f5115c906222a428e45a12128b6695b26dcf
ananya <ananyay.work@gmail.com>
2018-10-12 22:51:11 -0500

    deleting the extra lines

--- scrape_mars.py
+++ scrape_mars.py
@@ -9,7 +9,7 @@ import time
 
 
 def init_browser():
-    executable_path = {'executable_path': 'chromedriver.exe'}
+    executable_path = {'executable_path':'chromedriver.exe'}
     return Browser('chrome',**executable_path,headless=False)
 
 
